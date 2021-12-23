f39f6cf4d013f14e371aadeb4462d5578fce9769
AMALJELI <Aml3mr@hotmail.com>
2021-12-23 16:27:59 +0300

    init x code project structure

---
+++ Card-Reminder/Card-Reminder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Card-Reminder
+//
+//  Created by Amal Jeli on 19/05/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
